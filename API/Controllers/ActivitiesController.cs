d2b9433b37c8b11c1e7ab214f449996f29be8b14
mww1987 <S.Mojtaba1987@gmail.com>
2021-09-28 14:47:45 +0330

    end of 3

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
@@ -17,7 +19,14 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
+            return await _context.Activities.ToListAsync();
 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
         }
     }
 }
